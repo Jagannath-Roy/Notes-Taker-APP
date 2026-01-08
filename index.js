ee9a0f5ab04947ed45ad0eb1a7b55ed1fea58c6d
jagannathroy <jagaroy10@gmail.com>
2026-01-08 23:03:02 +0530

    Changed index.js

--- index.js
+++ index.js
@@ -43,4 +43,9 @@ app.post("/create",function(req,res){
    
  })
 
-app.listen(3000);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
